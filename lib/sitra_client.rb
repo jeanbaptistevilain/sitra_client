283fe67aed468a044ed11f302cac6df2814afb14
jeanbaptistevilain <jbvilain@gmail.com>
2014-04-25 16:59:25 +0200

    Support selection-based query

--- lib/sitra_client.rb
+++ lib/sitra_client.rb
@@ -1,5 +1,35 @@
-require "sitra_client/version"
+require 'sitra_client/version'
+require 'sitra_client/sitra_query'
+require 'open-uri'
 
 module SitraClient
-  # Your code goes here...
+
+  # Configuration defaults
+  @config = {
+      :base_url => 'http://api.sitra-tourisme.com/api/v001',
+      :api_key => '',
+      :site_identifier => ''
+  }
+
+  @valid_config_keys = @config.keys
+
+  # Configure through hash
+  def self.configure(opts = {})
+    opts.each {|k,v| @config[k.to_sym] = v if @valid_config_keys.include? k.to_sym}
+  end
+
+  def self.config
+    @config
+  end
+
+  def self.query(criteria = {})
+    results = []
+    query = SitraQuery.new(@config[:api_key], @config[:site_identifier], criteria)
+    puts "#{@config[:base_url]}/recherche/list-objets-touristiques?query=#{query.to_params}"
+    open("#{@config[:base_url]}/recherche/list-objets-touristiques?query=#{CGI.escape query.to_params}") { |f|
+      f.each_line {|line| results << line}
+    }
+    results
+  end
+
 end
--- sitra_client.gemspec
+++ sitra_client.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = SitraClient::VERSION
   spec.authors       = ["jeanbaptistevilain"]
   spec.email         = ["jbvilain@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{Sitra Client Gem}
+  spec.summary       = %q{This gem wraps the SITRA JSON API}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "activemodel", "3.2.14"
 end
